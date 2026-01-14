986b528041ea425634f7ea98730a7e6083cd464a
Liam Armstrong <lla.armstrong00@gmail.com>
2026-01-14 18:25:52 -0500

    add index.js and updated index.html

---
+++ index.js
@@ -0,0 +1,18 @@
+let code1;
+code1 = 7 + 3;
+
+let code2;
+code2 = 60 - 40 / 2;
+
+let code3;
+code3 = (4 % 2) + 39;
+
+const message = "The vault has been secured. The combination is:";
+
+let codeA = code1 + code2 + code3;
+
+let codeB = "${code1}-${code2}-${code3}";
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
